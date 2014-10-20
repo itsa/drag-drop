06ad4dacd423598bc2fcd690c11e8b52eb5ea890
Marco Asbreuk <marco@itsasbreuk.nl>
2014-10-20 23:02:49 +0200

    intermediate saving

--- tests/drag-drop.js
+++ tests/drag-drop.js
@@ -6,9 +6,84 @@ var expect = require('chai').expect,
     should = require('chai').should();
 
 
-describe('Testgroup', function () {
+describe('Drag and drop Elements', function () {
 
-    it('Your test', function () {
+    it('Moved by delegated dragging from within dropzones', function () {
+    });
+
+    it('Delegated dragging from within dropzones adds the right attributes', function () {
+    });
+
+    it('UI:dropzone-over event when dragged over dropzone', function () {
+    });
+
+    it('UI:dropzone-out event when dragged out of dropzone', function () {
+    });
+
+    it('UI:dropzone-drop event when dropped inside dropzone', function () {
+    });
+
+    it('testEmitter:dropzone-over event when dragged over dropzone', function () {
+    });
+
+    it('testEmitter:dropzone-out event when dragged out of dropzone', function () {
+    });
+
+    it('testEmitter:dropzone-drop event when dropped inside dropzone', function () {
+    });
+
+    it('No *:dropzone-over event when dragged over invalid dropzone', function () {
+    });
+
+    it('e.dropzone Promise during draggin over dropzone and moving out of dropzone', function () {
+    });
+
+    it('e.dropzone Promise during draggin over dropzone and dropped inside dropzone', function () {
+    });
+
+    it('Different attributes on start of copying item', function () {
+    });
+
+    it('Different attributes on switching from move to copying item', function () {
+    });
+
+    it('Right classes on start of copying item', function () {
+    });
+
+    it('Right classes on switching from move to copying item', function () {
+    });
+
+    it('Unable dropping of movable draggable inside copy-dropzone', function () {
+    });
+
+    it('Unable dropping of copyable draggable inside move-dropzone', function () {
+    });
+
+    it('Dropping of movable draggable inside move-dropzone', function () {
+    });
+
+    it('Dropping of copy draggable inside copy-dropzone', function () {
+    });
+
+    it('Dropping of movable draggable inside all-dropzone', function () {
+    });
+
+    it('Dropping of copy draggable inside all-dropzone', function () {
+    });
+
+    it('Unable dropping of non-emitter draggable inside emitter-dropzone', function () {
+    });
+
+    it('Unable dropping of wrong emitter draggable inside emitter-dropzone', function () {
+    });
+
+    it('Dropping of emitter draggable inside emitter-dropzone', function () {
+    });
+
+    it('Unable dropping of draggable without dd-dropzone and dd-emitter inside all-dropzone', function () {
+    });
+
+    it('No drop with a changed dd-drop defaultFn', function () {
     });
 
 });
