ddc86cab8653c9fd4e0688f932455ae71768a15b
Marco Asbreuk <marco@itsasbreuk.nl>
2015-02-24 00:12:39 +0100

    intermediate saving

--- drag-drop.js
+++ drag-drop.js
@@ -244,6 +244,7 @@ module.exports = function (window) {
         DD, DD_Object;
 
     require('vdom')(window);
+    require('node-plugin')(window);
     require('window-ext')(window);
 
     DD = {
